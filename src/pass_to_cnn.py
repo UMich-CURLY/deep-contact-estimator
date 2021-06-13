8cfdc8ded739a7085e48f8e645ba0cb67926c375
Tingjun Li <tingjunl@umich.edu>
2021-06-13 15:46:09 -0400

    fixed a bug in pass_to_cnn.py

--- src/pass_to_cnn.py
+++ src/pass_to_cnn.py
@@ -12,38 +12,19 @@ from src.contact_cnn import *
 from utils.data_handler_np import *
 
 
-def compute_accuracy(dataloader, model):
-    num_correct = 0
-    num_data = 0
-    with torch.no_grad():
-        for sample in tqdm(dataloader):  # tqdm shows a smart progress meter
-            input_data = sample['data']
-            gt_label = sample['label']
-
-            output = model(input_data)
-            _, prediction = torch.max(output, 1)
-
-            num_data += input_data.size(0)
-            num_correct += (prediction == gt_label).sum().item()
-    return num_correct / num_data
-
-
 def compute_prediction(dataloader, model):
     num_correct = 0
     num_data = 0
     with torch.no_grad():
-        for sample in dataloader:  # tqdm shows a smart progress meter, but here we don't need it
-            input_data = sample['data']
-            gt_label = sample['label']
+        for input_data in dataloader:  # tqdm shows a smart progress meter, but here we don't need it
 
             output = model(input_data)
             _, prediction = torch.max(output, 1)
-            print("Ground_truth: ", gt_label)
             print("Prediction: ", prediction)
             return prediction
 
 
-def receive_msg(feature_msg, label_msg):
+def receive_input(feature_matrix):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('Using ', device)
 
@@ -54,8 +35,8 @@ def receive_msg(feature_msg, label_msg):
 
     config = yaml.load(open(args.config_name))
 
-    test_data = contact_dataset(feature_msg, label_msg, window_size=config['window_size'], device=device)
-    test_dataloader = DataLoader(dataset=test_data, batch_size=config['batch_size'])
+    test_data = contact_dataset(feature_matrix, device=device)
+    test_dataloader = DataLoader(dataset=test_data, batch_size=1)
 
     # init network
     model = contact_cnn()
@@ -63,9 +44,7 @@ def receive_msg(feature_msg, label_msg):
     model.load_state_dict(checkpoint['model_state_dict'])
     model = model.eval().to(device)
 
-    # test_acc = compute_accuracy(test_dataloader, model)
     test_predict = compute_prediction(test_dataloader, model)
 
-    # print("Test accuracy is: %.4f" % test_acc)
     print("Test prediction is: %.4f" % test_predict)
     return test_predict
