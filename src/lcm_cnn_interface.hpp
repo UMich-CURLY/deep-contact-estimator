ceff3b0e1c062a0efb24194052aed24b0a30264a
Tingjun-Li <tingjunl@umich.edu>
2021-07-16 18:09:18 -0400

    Changed directory on jetson

--- src/lcm_cnn_interface.hpp
+++ src/lcm_cnn_interface.hpp
@@ -37,7 +37,7 @@ std::queue<float *> cnnInputLegQueue;
 std::queue<float *> cnnInputIMUQueue;
 std::queue<int> cnnInputGtLabelQueue;
 // std::ofstream myfile;
-std::string PROGRAM_PATH = "/home/curly/Desktop/LCM_CNN_INTERFACE/deep-contact-estimator/";
+std::string PROGRAM_PATH = "/media/jetson256g/code/LCM_CNN_INTERFACE/deep-contact-estimator/";
 //!
 //! \brief The Handler class takes in LCM messages from subscribed channels and process them
 //! 
